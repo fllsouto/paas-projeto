a4144c03948a084a3fc6dd866e933299e2ba9577
Nicolas Amorim <nicolas.amorim@dampa.com.br>
2021-02-22 13:22:14 -0300

    remove dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,8 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './database/database.module';
 import { LoggerMiddleware } from './pings/middlewares/test.middleware';
-import { Ping } from './pings/ping.entity';
 import { PingsModule } from './pings/pings.module';
 
 @Module({
